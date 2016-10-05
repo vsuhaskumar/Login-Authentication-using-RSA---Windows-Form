08cedf99dd505faee8c6d65c4a83e3c705615306
Suhas V Kumar <suhas-kumar@uiowa.edu>
2016-10-05 09:47:33 -0500

    Added Windows Form - Login Authentiction

---
+++ Suhas/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Suhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.userlabel = new System.Windows.Forms.Label();
+            this.Passlabel = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.UValid = new System.Windows.Forms.Label();
+            this.PValid = new System.Windows.Forms.Label();
+            this.HelpLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // UserName
+            // 
+            this.UserName.Location = new System.Drawing.Point(259, 53);
+            this.UserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(215, 22);
+            this.UserName.TabIndex = 0;
+            this.UserName.TextChanged += new System.EventHandler(this.UserName_TextChanged);
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(259, 116);
+            this.Password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(215, 22);
+            this.Password.TabIndex = 1;
+            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // userlabel
+            // 
+            this.userlabel.AutoSize = true;
+            this.userlabel.Location = new System.Drawing.Point(95, 57);
+            this.userlabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.userlabel.Name = "userlabel";
+            this.userlabel.Size = new System.Drawing.Size(75, 17);
+            this.userlabel.TabIndex = 2;
+            this.userlabel.Text = "UserName";
+            // 
+            // Passlabel
+            // 
+            this.Passlabel.AutoSize = true;
+            this.Passlabel.Location = new System.Drawing.Point(95, 124);
+            this.Passlabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Passlabel.Name = "Passlabel";
+            this.Passlabel.Size = new System.Drawing.Size(69, 17);
+            this.Passlabel.TabIndex = 3;
+            this.Passlabel.Text = "Password";
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(259, 185);
+            this.Submit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(100, 28);
+            this.Submit.TabIndex = 4;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // UValid
+            // 
+            this.UValid.AutoSize = true;
+            this.UValid.Location = new System.Drawing.Point(512, 60);
+            this.UValid.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.UValid.Name = "UValid";
+            this.UValid.Size = new System.Drawing.Size(0, 17);
+            this.UValid.TabIndex = 5;
+            // 
+            // PValid
+            // 
+            this.PValid.AutoSize = true;
+            this.PValid.Location = new System.Drawing.Point(516, 123);
+            this.PValid.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PValid.Name = "PValid";
+            this.PValid.Size = new System.Drawing.Size(0, 17);
+            this.PValid.TabIndex = 6;
+            // 
+            // HelpLabel
+            // 
+            this.HelpLabel.AutoSize = true;
+            this.HelpLabel.Location = new System.Drawing.Point(95, 247);
+            this.HelpLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.HelpLabel.Name = "HelpLabel";
+            this.HelpLabel.Size = new System.Drawing.Size(37, 17);
+            this.HelpLabel.TabIndex = 7;
+            this.HelpLabel.TabStop = true;
+            this.HelpLabel.Text = "Help";
+            this.HelpLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.HelpLabel_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 324);
+            this.Controls.Add(this.HelpLabel);
+            this.Controls.Add(this.PValid);
+            this.Controls.Add(this.UValid);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.Passlabel);
+            this.Controls.Add(this.userlabel);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.UserName);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserName;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label userlabel;
+        private System.Windows.Forms.Label Passlabel;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Label UValid;
+        private System.Windows.Forms.Label PValid;
+        private System.Windows.Forms.LinkLabel HelpLabel;
+    }
+}
+
---
+++ Suhas/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Suhas
+{
+    public partial class Form1 : Form
+    {
+        int uflag;
+        int u_num;
+        int u_cap;
+        int pflag;
+        int p_cap;
+        int p_len;
+        int p_spl;
+        int p_num;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            uflag = 0;//Invalid
+            u_num = 0;//invalid
+            u_cap = 0;//invalid
+            pflag = 0; //Invalid
+            p_cap = 0;//invalid
+            p_len = 0;//invalid
+            p_spl = 0;//invalid
+            p_num = 0;//invalid
+        }
+
+        void setUFlag()
+        {
+            String uname = UserName.Text;
+            char[] un = uname.ToCharArray();
+           if (uname.Length < 5)
+                uflag = 0;
+          // for alphabet ( upper only )
+            for (int i = 0; i < uname.Length; i++)
+            {
+                if ((un[i] >= 65 && un[i] <= 90))
+                {
+                    u_cap = 1;
+                    break;
+                }
+                else
+                    u_cap = 0;
+
+            }
+            // for number
+            for(int i = 0; i < uname.Length ;i++ )
+            {
+                if (un[i] >= 48 && un[i] <= 57)
+                {
+                    u_num = 1;
+                    break;
+                }
+                else
+                    u_num = 0;
+            }
+            
+            // if uname has both number and capital letter and length >=5 
+            if (uname.Length >= 5 && u_cap == 1 && u_num == 1)
+                    uflag = 1;
+                else
+                    uflag = 0;
+            
+        }
+
+        void setPFlag()
+        {
+            String passw = Password.Text;
+            char[] pn = passw.ToCharArray();
+            if (pn.Length < 8)
+                p_len = 0;
+            else
+                p_len = 1;
+
+            //character is alphabet
+            for (int i = 0; i < passw.Length; i++)
+            {
+                if (pn[i] >= 65 && pn[i] <= 90)
+                {
+                    p_cap = 1;
+                    break;
+                }
+                else
+                    p_cap = 0;
+            }
+
+            for (int i = 0; i < passw.Length; i++)
+            {
+                // character is not an alphabet(upper and lower case) or a number
+                if(!(pn[i] >=65 && pn[i] <= 90) && !(pn[i] >= 97 && pn[i] <= 122 ) && !(pn[i] >= 48 && pn[i] <= 57))
+                {
+                    p_spl = 1;
+                    break;
+                }
+                else
+                    p_spl = 0;
+            }
+        //character is number
+            for (int i = 0; i < passw.Length; i++)
+            {
+                if (pn[i] >= 48 && pn[i] <= 57)
+                {
+                    p_num = 1;
+                    break;
+                }
+                else
+                    p_num = 0;
+            }
+
+            if ((p_spl == 1) && (p_len == 1) && (p_cap == 1) && (p_num ==1))
+                pflag = 1;
+            else
+                pflag = 0;
+
+            
+        }
+
+        private void UserName_TextChanged(object sender, EventArgs e)
+        {
+            setUFlag();
+            
+            if (uflag == 0)
+            {
+                UValid.Text = "Invalid!!!";
+                UValid.ForeColor = Color.Red;
+            }
+            else
+            {
+                UValid.Text = "Valid!!!";
+                UValid.ForeColor = Color.Green;
+            }
+
+            
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            setPFlag();
+
+            if (pflag == 0)
+            {
+                PValid.Text = "Invalid!!!";
+                PValid.ForeColor = Color.Red;
+            }
+            else
+            {
+                PValid.Text = "Valid!!!";
+                PValid.ForeColor = Color.Green;
+            }
+        }
+        int power(int m, int exp, int mod)
+        {
+            int res = 1;
+            for (int i = 0; i < exp; i++)
+                res = (res*m)%mod;
+            return res;
+        }
+        String RSA(String text)
+        {
+            char[] message = text.ToCharArray();
+            int e, n, phi, p = 19, q = 23;
+            n = p * q;
+            phi = (p - 1) * (q - 1);
+            e = 13;
+            n = 61;
+            int[] cipher = new int[text.Length];
+            for (int i = 0; i < text.Length; i++)
+                cipher[i] = power(message[i], e, n);
+            String res="";
+            for (int i = 0; i < cipher.Length; i++)
+                res += cipher[i].ToString();
+            return res;
+                
+        }
+
+        private void HelpLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show(" 1. User-name must be minimum 5 characters and have at least 1 capital letter and 1 number . \n 2.Password must be minimum 8 characters and have at least 1 Capital Letter , 1 Special character and 1 number.");
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            String cipher_uname = RSA(UserName.Text);
+            String cipher_passw = RSA(Password.Text);
+            MessageBox.Show("Encrypted UserName : " + cipher_uname + "\nEncrypted Password : " + cipher_passw);
+
+        }
+
+        
+    }
+}
